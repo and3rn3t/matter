151a9a6b254a72778d102a0a47570c492d3df27a
Matthew Anderson <andersonmatthewj@johndeere.com>
2021-08-23 12:22:47 -0500

    updated controllers to show index and edit views

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-const UserModel = require("../models").User;
+const Users = require("../models").Users;
+// const Groups = require("../models").Groups;
+// const Friendships = require("../models").Friendships;
+// const Posts = require("..models").Posts;
+
+// Index route
+router.get("/", (req, res) => {
+  Users.findAll().then((user) => {
+    res.render("users/index.ejs", {
+      users: user,
+    });
+  });
+});
 
 // GET USERS PROFILE
 router.get("/profile/:id", (req, res) => {
   console.log(req.user);
-  UserModel.findByPk(req.params.id).then((userProfile) => {
+  Users.findByPk(req.params.id).then((userProfile) => {
     res.render("users/profile.ejs", {
       user: userProfile,
     });
--- server.js
+++ server.js
@@ -2,41 +2,56 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const methodOverride = require("method-override");
-const jwt = require("jsonwebtoken");
-const cookieParser = require("cookie-parser");
 
-// make change
-
-const verifyToken = (req, res, next) => {
-  let token = req.cookies.jwt;
-  // COOKIE PARSER GIVES YOU A .cookies PROP, WE NAMED OUR TOKEN jwt
-  console.log("Cookies: ", req.cookies.jwt);
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, decodedUser) => {
-    if (err || !decodedUser) {
-      return res.status(401).json({ error: "Unauthorized Request" });
-    }
-    req.user = decodedUser;
-    // ADDS A .user PROP TO REQ FOR TOKEN USER
-    console.log(decodedUser);
-
-    next();
-  });
-};
-
-app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static("public"));
+
+// const jwt = require("jsonwebtoken");
+// const cookieParser = require("cookie-parser");
+
+// Log stuff
+// app.use((req, res, next) => {
+// 	let logStr = `${req.method} ${req.url}`;
+
+// 	if(Object.keys(req.body).length !== 0) {
+// 		logStr += ` -- DATA: ${JSON.stringify(req.body)}`;
+// 	}
+
+// 	console.log(logStr);
+// 	next();
+// });
+
+// const verifyToken = (req, res, next) => {
+//   let token = req.cookies.jwt;
+//   // COOKIE PARSER GIVES YOU A .cookies PROP, WE NAMED OUR TOKEN jwt
+//   console.log("Cookies: ", req.cookies.jwt);
+
+//   jwt.verify(token, process.env.JWT_SECRET, (err, decodedUser) => {
+//     if (err || !decodedUser) {
+//       return res.status(401).json({ error: "Unauthorized Request" });
+//     }
+//     req.user = decodedUser;
+//     // ADDS A .user PROP TO REQ FOR TOKEN USER
+//     console.log(decodedUser);
+
+//     next();
+//   });
+// };
+
+// app.use(cookieParser());
 
 // HOMEPAGE
 app.get("/", (req, res) => {
   res.render("users/index.ejs");
 });
 
-app.use("/auth", require("./controllers/authController.js"));
-app.use("/users", verifyToken, require("./controllers/usersController.js"));
+// app.use("/auth", require("./controllers/authController.js"));
+// app.use("/users", verifyToken, require("./controllers/usersController.js"));
 
-app.listen(process.env.PORT, () => {
+// Import controllers
+app.use("/users", require("./controllers/usersController.js"));
+
+app.listen(3002, () => {
   console.log("Nodemon listening");
 });
