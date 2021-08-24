52534aa20a5e9f548242422fe41631ea98acd0a4
Matthew Anderson <andersonmatthewj@johndeere.com>
2021-08-23 20:48:50 -0500

    updated server.js with env code

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const express = require("express");
 
 const app = express();
 const methodOverride = require("method-override");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static("public"));
-
-// const jwt = require("jsonwebtoken");
-// const cookieParser = require("cookie-parser");
 
 // Logging stuff
 app.use((req, res, next) => {
@@ -23,29 +22,27 @@ app.use((req, res, next) => {
   next();
 });
 
-// const verifyToken = (req, res, next) => {
-//   let token = req.cookies.jwt;
-//   // COOKIE PARSER GIVES YOU A .cookies PROP, WE NAMED OUR TOKEN jwt
-//   console.log("Cookies: ", req.cookies.jwt);
+const verifyToken = (req, res, next) => {
+  let token = req.cookies.jwt;
+  console.log("Cookies: ", req.cookies.jwt);
 
-//   jwt.verify(token, process.env.JWT_SECRET, (err, decodedUser) => {
-//     if (err || !decodedUser) {
-//       return res.status(401).json({ error: "Unauthorized Request" });
-//     }
-//     req.user = decodedUser;
-//     // ADDS A .user PROP TO REQ FOR TOKEN USER
-//     console.log(decodedUser);
+  jwt.verify(token, process.env.JWT_SECRET, (err, decodedUser) => {
+    if (err || !decodedUser) {
+      return res.status(401).json({ error: "Unauthorized Request" });
+    }
+    req.user = decodedUser;
+    console.log(decodedUser);
 
-//     next();
-//   });
-// };
+    next();
+  });
+};
 
-// app.use(cookieParser());
+app.use(cookieParser());
 
 // HOMEPAGE
-// app.get("/", (req, res) => {
-//   res.render("users/index.ejs");
-// });
+app.get("/", (req, res) => {
+  res.render("users/index.ejs");
+});
 
 // app.use("/auth", require("./controllers/authController.js"));
 // app.use("/users", verifyToken, require("./controllers/usersController.js"));
